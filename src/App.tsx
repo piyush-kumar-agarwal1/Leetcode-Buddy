3c55dc9994680cb8aabfd56cd581e44a676b918e
Piyush Kumar Agarwal <chatwithapiyush@gmail.com>
2025-03-22 04:46:26 +0530

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
